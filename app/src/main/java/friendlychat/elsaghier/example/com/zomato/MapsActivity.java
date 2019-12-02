4786fddc24e2b275df78bf3464a227076b8f2384
EL_Saghier <thedreamereng@gmail.com>
2019-12-02 16:42:14 +0200

    migration to androidx

--- app/src/main/java/friendlychat/elsaghier/example/com/zomato/MapsActivity.java
+++ app/src/main/java/friendlychat/elsaghier/example/com/zomato/MapsActivity.java
@@ -1,7 +1,7 @@
 package friendlychat.elsaghier.example.com.zomato;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import androidx.fragment.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
