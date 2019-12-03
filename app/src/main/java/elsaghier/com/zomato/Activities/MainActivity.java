7059548cbb54331db1ec9fa7e74ae8bc64178b06
EL_Saghier <thedreamereng@gmail.com>
2019-12-03 02:11:22 +0200

    remove volley dependence from gradle

--- app/src/main/java/elsaghier/com/zomato/Activities/MainActivity.java
+++ app/src/main/java/elsaghier/com/zomato/Activities/MainActivity.java
@@ -1,30 +1,18 @@
 package elsaghier.com.zomato.Activities;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import elsaghier.com.zomato.Adapter.RestaurantAdapter;
 import elsaghier.com.zomato.Model.RestaurantModel;
